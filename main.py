5578000cd8509f17d764de4485f1f2cea485423b
Nircek <nircek-2103@protonmail.com>
2018-08-16 17:44:14 +0200

    Add connections between elements

--- main.py
+++ main.py
@@ -54,8 +54,10 @@ class ANDgate(element):
     for i in range(len(self.inputs)):
       j = 40*(i+1)/(len(self.inputs)+1)
       g.w.create_line(self.x, self.y+j, self.x-10, self.y+j, fill=self.s)
+      x, y = g.get(self.inputs[i]).xy()
+      g.w.create_line(x, y, self.x-10, self.y+j, fill=g.getPowerColor(self.inputs[i], self.UUID))
   def xy(self):
-    return (self.x+self.W//2, self.y+self.H//2)
+    return (self.x+self.W, self.y+self.H//2)
 
 class ORgate(element):
   slots = -1
@@ -79,6 +81,10 @@ class ORgate(element):
       j = 40*(i+1)/(len(self.inputs)+1)
       k = math.sqrt(20**2*2-(j-20)**2)-20
       g.w.create_line(self.x+k, self.y+j, self.x-10, self.y+j, fill=self.s)
+      x, y = g.get(self.inputs[i]).xy()
+      g.w.create_line(x, y, self.x-10, self.y+j, fill=g.getPowerColor(self.inputs[i], self.UUID))
+  def xy(self):
+    return (self.x+self.W, self.y+self.H//2)
 
 class NOTgate(element):
   slots = 1
@@ -96,8 +102,16 @@ class NOTgate(element):
     g.w.create_line(self.x, self.y+40, self.x+32, self.y+20, fill=self.s)
     g.arc(self.x+36, self.y+20, 4, 0, 360, self.s)
     g.w.create_line(self.x, self.y+20, self.x-10, self.y+20, fill=self.s)
+    x, y = g.get(self.inputs[0]).xy()
+    g.w.create_line(x, y, self.x, self.y+20, fill=g.getPowerColor(self.inputs[0], self.UUID))
+  def xy(self):
+    return (self.x+self.W, self.y+self.H//2)
 
 class UUIDs:
+  def getPowerColor(self, s, d):
+    if self.get(s).power:
+      return 'red'
+    return 'black'
   def arc(self,x,y,r,s,e, outline='black'):
     if e >= 360:
       self.w.create_arc(x-r,y-r,x+r,y+r,start=0,extent=180,style=ARC,outline=outline)
@@ -127,51 +141,58 @@ class UUIDs:
     return self.UUIDi
   def update(self, x=None):
     if x is None:
-      for e in self.UUIDS:
-        self.update(e.UUID)
+      for i in range(len(self.UUIDS)):
+        tt = False
+        for e in self.UUIDS:
+          t = self.get(e.UUID).power
+          self.update(e.UUID)
+          if self.get(e.UUID).power != t:
+            tt = True
+        if not tt:
+          break
+      self.tk.update()
+      input()
     else:
       s = self.get(x)
       i = []
       for j in s.inputs:
         i += [self.get(j).power]
       s.update(i)
+      UUIDS.render()
   def render(self):
     for e in self.UUIDS:
       e.render(self)
     self.tk.update()
 
 UUIDS = UUIDs()
-s = 50
+s = 70
 in1 = UUIDS.new(element(s,s,'in1'))
 in2 = UUIDS.new(element(s,2*s,'in2'))
 not1 = UUIDS.new(NOTgate(2*s,s,'not1', [in1]))
 not2 = UUIDS.new(NOTgate(2*s,2*s,'not2', [in2]))
 orc1 = UUIDS.new(ANDgate(3*s,1.5*s,'orc1', [not1, not2]))
 or1 = UUIDS.new(ORgate(4*s,s,'or1', [not1, orc1]))
-or2 = UUIDS.new(ORgate(4*s,2*s,'or2', [not2, orc1, in1]))
+or2 = UUIDS.new(ORgate(4*s,2*s,'or2', [orc1, not2]))
 no1 = UUIDS.new(NOTgate (5*s,s,'no1', [or1]))
 no2 = UUIDS.new(NOTgate(5*s,2*s,'no2', [or2]))
 orc2 = UUIDS.new(ORgate(6*s,1.5*s,'orc2', [no1, no2]))
-UUIDS.get(in1).power = False
-UUIDS.get(in2).power = False
-for i in range(5):
+while 1:
+  UUIDS.get(in1).power = False
+  UUIDS.get(in2).power = False
   UUIDS.update()
-print(UUIDS.get(in1).power, UUIDS.get(in2).power, UUIDS.get(orc2).power)
-UUIDS.get(in1).power = False
-UUIDS.get(in2).power = True
-for i in range(5):
+  print(UUIDS.get(in1).power, UUIDS.get(in2).power, UUIDS.get(orc2).power)
+  UUIDS.get(in1).power = False
+  UUIDS.get(in2).power = True
   UUIDS.update()
-print(UUIDS.get(in1).power, UUIDS.get(in2).power, UUIDS.get(orc2).power)
-UUIDS.get(in1).power = True
-UUIDS.get(in2).power = False
-for i in range(5):
+  print(UUIDS.get(in1).power, UUIDS.get(in2).power, UUIDS.get(orc2).power)
+  UUIDS.get(in1).power = True
+  UUIDS.get(in2).power = False
   UUIDS.update()
-print(UUIDS.get(in1).power, UUIDS.get(in2).power, UUIDS.get(orc2).power)
-UUIDS.get(in1).power = True
-UUIDS.get(in2).power = True
-for i in range(5):
+  print(UUIDS.get(in1).power, UUIDS.get(in2).power, UUIDS.get(orc2).power)
+  UUIDS.get(in1).power = True
+  UUIDS.get(in2).power = True
   UUIDS.update()
-print(UUIDS.get(in1).power, UUIDS.get(in2).power, UUIDS.get(orc2).power)
+  print(UUIDS.get(in1).power, UUIDS.get(in2).power, UUIDS.get(orc2).power)
 while 1:
   UUIDS.render()
 code.InteractiveConsole(vars()).interact()
