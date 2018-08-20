d33242bada46fb5591f595b38435d3b74294fcbb
Nircek <nircek-2103@protonmail.com>
2018-08-20 16:35:58 +0200

    Add pos class

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import code
 from tkinter import *
 import math
 
-
+class pos:
+  def __init__(self, *a):
+    if len(a) == 1:
+      a = a[0]
+    self.x = a[0]
+    self.y = a[1]
+    self.w = a[0]
+    self.h = a[1]
+    
 class element:
   slots = 0
-  W = 10
-  H = 10
+  s = pos(10, 10)
   def __str__(self):
     return 'element'
-  def __init__(self, parent, x, y, name=None, ins=None, s='black'):
+  def __init__(self, parent, p, name=None, ins=None, st='black'):
     self.addr = super().__repr__().split('0x')[1][:-1]
     self.UUID = -1
     self.updates = 0
@@ -25,12 +32,11 @@ class element:
     for e in self.inputs:
       self.parent.get(e).outs += [self]
     self.power = False
-    self.x = x
-    self.y = y
-    self.s = s
+    self.p = p
+    self.st = st
     self.outs = []
   def getsize(self):
-    return self.W, self.H
+    return self.s
   def update(self, ins):
     self.power = self.calc(ins)
     self.updates += 1
@@ -41,12 +47,12 @@ class element:
       self.s = 'black'
   def onclick2(self):
       self.s = 'green'
-  def motion(self, x, y):
-      self.x = x - self.W // 2
-      self.y = y - self.H // 2
+  def motion(self, p):
+      self.p.x = p.x - self.s.w // 2
+      self.p.y = p.y - self.s.h // 2
   def onkey(self, ev):
-    if  ev.x >= self.x and ev.x <= self.x+self.W \
-    and ev.y >= self.y and ev.y <= self.y+self.H:
+    if  ev.x >= self.p.x and ev.x <= self.p.x+self.s.w \
+    and ev.y >= self.p.y and ev.y <= self.p.y+self.s.h:
       print(self.UUID)
       if ev.keycode == ord('D'):
         self.inputs = []
@@ -69,29 +75,27 @@ class element:
   def calc(self, ins):
     return self.power
   def render(self):
-    self.parent.arc(self.x+self.W//2, self.y+self.H//2, min(self.W, self.H)//2, 0, 360, self.s)
+    self.parent.arc(self.p.x+self.s.w//2, self.p.y+self.s.h//2, min(self.s.w, self.s.h)//2, 0, 360, self.st)
   def xy(self):
-    return (self.x+self.W//2, self.y+self.H//2)
+    return (self.p.x+self.s.w//2, self.p.y+self.s.h//2)
 
 
 class switch(element):
   slots = 0
-  W = 40
-  H = 40
+  s = pos(40, 40)
   def __str__(self):
     return 'switch'
   def onclick2(self):
     self.power = not self.power
   def render(self):
-    s = self.s
-    if s == 'black' and self.power:
-      s = 'red'
-    self.parent.arc(self.x+self.W//2, self.y+self.H//2, min(self.W, self.H)//2, 0, 360, s)
+    st = self.st
+    if st == 'black' and self.power:
+      st = 'red'
+    self.parent.arc(self.p.x+self.s.w//2, self.p.y+self.s.h//2, min(self.s.w, self.s.h)//2, 0, 360, st)
 
 class light(element):
   slots = 1
-  H = 40
-  W = 40
+  s = pos(40, 40)
   def __str__(self):
     return 'light'
   def calc(self, ins):
@@ -100,18 +104,17 @@ class light(element):
     else:
       return ins[0]
   def render(self):
-    s = self.s
-    if s == 'black' and self.power:
-      s = 'red'
-    self.parent.w.create_rectangle(self.x, self.y, self.x+self.W, self.y+self.H, outline=s)
+    st = self.st
+    if st == 'black' and self.power:
+      st = 'red'
+    self.parent.w.create_rectangle(self.p.x, self.p.y, self.p.x+self.s.w, self.p.y+self.s.h, outline=st)
     if len(self.inputs) >= self.slots:
       pc = self.parent.getPowerColor(self.inputs[0], self.UUID)
       x, y = self.parent.get(self.inputs[0]).xy()
-      self.parent.w.create_line(x, y, self.x+self.W//2, self.y+self.H//2, fill=pc)
+      self.parent.w.create_line(x, y, self.p.x+self.s.w//2, self.p.y+self.s.h//2, fill=pc)
 
 class ANDgate(element):
-  W = 40
-  H = 40
+  s = pos(40, 40)
   slots = -1
   def __str__(self):
     return 'ANDgate'
@@ -121,23 +124,22 @@ class ANDgate(element):
         return False
     return True
   def render(self):
-    self.parent.w.create_line(self.x, self.y, self.x+20, self.y, fill=self.s)
-    self.parent.w.create_line(self.x, self.y, self.x, self.y+40, fill=self.s)
-    self.parent.w.create_line(self.x, self.y+40, self.x+20, self.y+40, fill=self.s)
-    self.parent.arc(self.x+20, self.y+20, 20, 270, 180, self.s)
+    self.parent.w.create_line(self.p.x, self.p.y, self.p.x+20, self.p.y, fill=self.st)
+    self.parent.w.create_line(self.p.x, self.p.y, self.p.x, self.p.y+40, fill=self.st)
+    self.parent.w.create_line(self.p.x, self.p.y+40, self.p.x+20, self.p.y+40, fill=self.st)
+    self.parent.arc(self.p.x+20, self.p.y+20, 20, 270, 180, self.st)
     for i in range(len(self.inputs)):
       j = 40*(i+1)/(len(self.inputs)+1)
       pc = self.parent.getPowerColor(self.inputs[i], self.UUID)
-      self.parent.w.create_line(self.x, self.y+j, self.x-10, self.y+j, fill=pc)
+      self.parent.w.create_line(self.p.x, self.p.y+j, self.p.x-10, self.p.y+j, fill=pc)
       x, y = self.parent.get(self.inputs[i]).xy()
-      self.parent.w.create_line(x, y, self.x-10, self.y+j, fill=pc)
+      self.parent.w.create_line(x, y, self.p.x-10, self.p.y+j, fill=pc)
   def xy(self):
-    return (self.x+self.W, self.y+self.H//2)
+    return (self.p.x+self.s.w, self.p.y+self.s.h//2)
 
 class ORgate(element):
   slots = -1
-  W = 40
-  H = 40
+  s = pos(40, 40)
   def __str__(self):
     return 'ORgate'
   def calc(self, ins):
@@ -146,24 +148,23 @@ class ORgate(element):
         return True
     return False
   def render(self):
-    self.parent.arc(self.x-20,self.y+20,math.sqrt(2*20**2),315,90,self.s)
-    self.parent.w.create_line(self.x, self.y, self.x+20, self.y, fill=self.s)
-    self.parent.w.create_line(self.x, self.y+40, self.x+20, self.y+40, fill=self.s)
-    self.parent.arc(self.x+20,self.y+20,20,270,180,self.s)
+    self.parent.arc(self.p.x-20,self.p.y+20,math.sqrt(2*20**2),315,90,self.st)
+    self.parent.w.create_line(self.p.x, self.p.y, self.p.x+20, self.p.y, fill=self.st)
+    self.parent.w.create_line(self.p.x, self.p.y+40, self.p.x+20, self.p.y+40, fill=self.st)
+    self.parent.arc(self.p.x+20,self.p.y+20,20,270,180,self.st)
     for i in range(len(self.inputs)):
       j = 40*(i+1)/(len(self.inputs)+1)
       k = math.sqrt(20**2*2-(j-20)**2)-20
       pc = self.parent.getPowerColor(self.inputs[i], self.UUID)
-      self.parent.w.create_line(self.x+k, self.y+j, self.x-10, self.y+j, fill=pc)
+      self.parent.w.create_line(self.p.x+k, self.p.y+j, self.p.x-10, self.p.y+j, fill=pc)
       x, y = self.parent.get(self.inputs[i]).xy()
-      self.parent.w.create_line(x, y, self.x-10, self.y+j, fill=pc)
+      self.parent.w.create_line(x, y, self.p.x-10, self.p.y+j, fill=pc)
   def xy(self):
-    return (self.x+self.W, self.y+self.H//2)
+    return (self.p.x+self.s.w, self.p.y+self.s.h//2)
 
 class NOTgate(element):
   slots = 1
-  W = 40
-  H = 40
+  s = pos(40, 40)
   def __str__(self):
     return 'NOTgate'
   def calc(self, ins):
@@ -172,17 +173,17 @@ class NOTgate(element):
     else:
       return not ins[0]
   def render(self):
-    self.parent.w.create_line(self.x, self.y, self.x, self.y+40, fill=self.s)
-    self.parent.w.create_line(self.x, self.y, self.x+32, self.y+20, fill=self.s)
-    self.parent.w.create_line(self.x, self.y+40, self.x+32, self.y+20, fill=self.s)
-    self.parent.arc(self.x+36, self.y+20, 4, 0, 360, self.s)
+    self.parent.w.create_line(self.p.x, self.p.y, self.p.x, self.p.y+40, fill=self.st)
+    self.parent.w.create_line(self.p.x, self.p.y, self.p.x+32, self.p.y+20, fill=self.st)
+    self.parent.w.create_line(self.p.x, self.p.y+40, self.p.x+32, self.p.y+20, fill=self.st)
+    self.parent.arc(self.p.x+36, self.p.y+20, 4, 0, 360, self.st)
     if len(self.inputs) >= self.slots:
       pc = self.parent.getPowerColor(self.inputs[0], self.UUID)
-      self.parent.w.create_line(self.x, self.y+20, self.x-10, self.y+20, fill=pc)
+      self.parent.w.create_line(self.p.x, self.p.y+20, self.p.x-10, self.p.y+20, fill=pc)
       x, y = self.parent.get(self.inputs[0]).xy()
-      self.parent.w.create_line(x, y, self.x-9, self.y+20, fill=pc)
+      self.parent.w.create_line(x, y, self.p.x-9, self.p.y+20, fill=pc)
   def xy(self):
-    return (self.x+self.W, self.y+self.H//2)
+    return (self.p.x+self.s.w, self.p.y+self.s.h//2)
 
 class UUIDs:
   def getPowerColor(self, s, d):
@@ -225,8 +226,8 @@ class UUIDs:
         del self.UUIDS[i]
       else:
         i += 1
-  def new(self, c, x, y, ins=None):
-    e = c(self, x, y, ins=ins)
+  def new(self, c, p, ins=None):
+    e = c(self, p, ins=ins)
     self.UUIDi += 1
     while self.get(self.UUIDi) is not None:
       self.UUIDi += 1
@@ -271,20 +272,20 @@ class UUIDs:
       e.render()
     if self.selected is not None and self.rmx != -1:
       t = self.get(self.selected)
-      self.w.create_line(t.x+t.W//2, t.y+t.H//2, self.rmx, self.rmy)
+      self.w.create_line(t.p.x+t.s.w//2, t.p.y+t.s.h//2, self.rmx, self.rmy)
     self.tk.update()
   def onclick1(self, ev):
     self.click_moved = False
     self.in_motion = None
     for e in self.UUIDS:
-      if  ev.x >= e.x and ev.x <= e.x+e.W \
-      and ev.y >= e.y and ev.y <= e.y+e.H:
+      if  ev.x >= e.p.x and ev.x <= e.p.x+e.s.w \
+      and ev.y >= e.p.y and ev.y <= e.p.y+e.s.h:
         self.in_motion = e.UUID
   def onrel1(self,ev):
     if not self.click_moved:
       for e in self.UUIDS:
-        if  ev.x >= e.x and ev.x <= e.x+e.W \
-        and ev.y >= e.y and ev.y <= e.y+e.H:
+        if  ev.x >= e.p.x and ev.x <= e.p.x+e.s.w \
+        and ev.y >= e.p.y and ev.y <= e.p.y+e.s.h:
           e.onclick1()
   def motion1(self, ev):
     self.click_moved = True
@@ -294,19 +295,19 @@ class UUIDs:
     self.click_moved = False
     self.selected = None
     for e in self.UUIDS:
-      if  ev.x >= e.x and ev.x <= e.x+e.W \
-      and ev.y >= e.y and ev.y <= e.y+e.H:
+      if  ev.x >= e.p.x and ev.x <= e.p.x+e.s.w \
+      and ev.y >= e.p.y and ev.y <= e.p.y+e.s.h:
         self.selected = e.UUID
   def onrel2(self,ev):
     if not self.click_moved:
       for e in self.UUIDS:
-        if  ev.x >= e.x and ev.x <= e.x+e.W \
-        and ev.y >= e.y and ev.y <= e.y+e.H:
+        if  ev.x >= e.p.x and ev.x <= e.p.x+e.s.w \
+        and ev.y >= e.p.y and ev.y <= e.p.y+e.s.h:
           e.onclick2()
     elif self.selected is not None:
       for e in self.UUIDS:
-        if  ev.x >= e.x and ev.x <= e.x+e.W \
-        and ev.y >= e.y and ev.y <= e.y+e.H:
+        if  ev.x >= e.p.x and ev.x <= e.p.x+e.s.w \
+        and ev.y >= e.p.y and ev.y <= e.p.y+e.s.h:
           e.inputs += [self.selected]
           self.get(self.selected).outs += [e]
     self.rmx = -1
@@ -319,7 +320,7 @@ class UUIDs:
     if ev.keycode > 111 and ev.keycode < 111+13:
       gates = [None, switch, light, NOTgate, ORgate, ANDgate]
       b = gates[ev.keycode-111]
-      self.new(b, ev.x-b.W//2, ev.y-b.H//2)
+      self.new(b, pos(ev.x-b.s.w//2, ev.y-b.s.h//2))
     if ev.keycode == 220:
       code.InteractiveConsole(vars()).interact()
     if ev.state == 0x40001:
@@ -330,17 +331,17 @@ class UUIDs:
 
 UUIDS = UUIDs()
 s = 70
-in1 = UUIDS.new(switch,s,s)
-in2 = UUIDS.new(switch,s,2*s)
-not1 = UUIDS.new(NOTgate,2*s,s,[in1])
-not2 = UUIDS.new(NOTgate,2*s,2*s,[in2])
-orc1 = UUIDS.new(ORgate,3*s,1.5*s,[not1, not2])
-or1 = UUIDS.new(ORgate,4*s,s,[not1, orc1])
-or2 = UUIDS.new(ORgate,4*s,2*s,[orc1, not2])
-no1 = UUIDS.new(NOTgate,5*s,s,[or1])
-no2 = UUIDS.new(NOTgate,5*s,2*s,[or2])
-orc2 = UUIDS.new(ORgate,6*s,1.5*s,[no1, no2])
-out = UUIDS.new(light, 7*s, 1.5*s, [orc2])
+in1 = UUIDS.new(switch,pos(s,s))
+in2 = UUIDS.new(switch,pos(s,2*s))
+not1 = UUIDS.new(NOTgate,pos(2*s,s),[in1])
+not2 = UUIDS.new(NOTgate,pos(2*s,2*s),[in2])
+orc1 = UUIDS.new(ORgate,pos(3*s,1.5*s),[not1, not2])
+or1 = UUIDS.new(ORgate,pos(4*s,s),[not1, orc1])
+or2 = UUIDS.new(ORgate,pos(4*s,2*s),[orc1, not2])
+no1 = UUIDS.new(NOTgate,pos(5*s,s),[or1])
+no2 = UUIDS.new(NOTgate,pos(5*s,2*s),[or2])
+orc2 = UUIDS.new(ORgate,pos(6*s,1.5*s),[no1, no2])
+out = UUIDS.new(light, pos(7*s, 1.5*s), [orc2])
 while 1:
   UUIDS.update()
   UUIDS.render()
